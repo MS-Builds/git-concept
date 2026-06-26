d6457472f9ba336e62dc8aa5ff69022d6432b755
MohammedSadiqOfficial <mohammedsadiqofficial.dev@gmail.com>
2026-06-26 13:01:47 +0530

    feature(signup): Added signup functionality with signup-button

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const loginBtn = document.getElementById("login");
+const signUpBtn = document.getElementById("signup");
 
 loginBtn.addEventListener("click", () => {
     alert("Logged In Successfully")
+})
+
+// SignUp
+signUpBtn.addEventListener("click",()=>{
+    alert("SignUp Successfully")
 })
