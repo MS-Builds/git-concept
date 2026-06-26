35d7288c8e96ca87d0e250f8727a1de7547e5ff6
MohammedSadiqOfficial <mohammedsadiqofficial.dev@gmail.com>
2026-06-26 13:06:10 +0530

    feature(payment): Added Payment Successfully

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const loginBtn = document.getElementById("login");
 const signUpBtn = document.getElementById("signup");
+const paymentBtn = document.getElementById("payment");
 
 loginBtn.addEventListener("click", () => {
     alert("Logged In Successfully")
 })
 
 // SignUp
-signUpBtn.addEventListener("click",()=>{
+signUpBtn.addEventListener("click", () => {
     alert("SignUp Successfully")
+})
+
+// Payment
+paymentBtn.addEventListener("click", () => {
+    alert("Payment completed successfully")
 })
